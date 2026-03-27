ef5c8d79439b428759964f7a153d6c88ffde2bd3
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-03-28 01:03:19 +0200

    Excersice 2

--- Program.cs
+++ Program.cs
@@ -5,37 +5,53 @@
         static void Main(string[] args)
         {
             #region Excersice 1
-            List<int> grades = [85, 92, 78, 95, 88, 70, 100, 65];
-            Console.WriteLine(string.Join(", ", grades));
-            Console.WriteLine($"Count {grades.Count}");
-            Console.WriteLine($"first : {grades[0]}");
-            Console.WriteLine($"last : {grades[^1]}");
-            grades.Sort();
+            //List<int> grades = [85, 92, 78, 95, 88, 70, 100, 65];
+            //Console.WriteLine(string.Join(", ", grades));
+            //Console.WriteLine($"Count {grades.Count}");
+            //Console.WriteLine($"first : {grades[0]}");
+            //Console.WriteLine($"last : {grades[^1]}");
+            //grades.Sort();
 
-            foreach (int grade in grades)
-            {
-                Console.WriteLine(grade);
-            }
-            Console.WriteLine($"first grade above 90 : {grades.Find(x => x > 90)}");
-            var failingGrades = grades.FindAll(x => x < 75);
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+            //Console.WriteLine($"first grade above 90 : {grades.Find(x => x > 90)}");
+            //var failingGrades = grades.FindAll(x => x < 75);
 
-            Console.WriteLine("Failing grades:");
-            foreach (var g in failingGrades)
-            {
-                Console.WriteLine(g);
-            }
-            Console.WriteLine($"Remove all failing grades (below 75) {grades.RemoveAll(x => x < 75)}");
-            Console.WriteLine($"Check if any grade equals 100 {grades.Exists(x => x == 100)}");
-           List<string> Strings = new List<string>();
-             foreach (int grade in grades)
-            {
-                Strings.Add("Grade: " + grade);
-            }
+            //Console.WriteLine("Failing grades:");
+            //foreach (var g in failingGrades)
+            //{
+            //    Console.WriteLine(g);
+            //}
+            //Console.WriteLine($"Remove all failing grades (below 75) {grades.RemoveAll(x => x < 75)}");
+            //Console.WriteLine($"Check if any grade equals 100 {grades.Exists(x => x == 100)}");
+            //List<string> Strings = new List<string>();
+            //foreach (int grade in grades)
+            //{
+            //    Strings.Add("Grade: " + grade);
+            //}
 
-            foreach (string g in Strings)
-            {
-                Console.WriteLine(g);
-            }
+            //foreach (string g in Strings)
+            //{
+            //    Console.WriteLine(g);
+            //}
+            #endregion
+            #region Excersice 2
+           // SortedList<int, string> scores = new()
+           // { [500] = "Ahmed", [200] = "Sara", [800] = "Ali", [350] = "Mona" };
+           // foreach (var entry in scores)
+           // {
+           //     Console.WriteLine($"{entry.Value} : {entry.Key}");
+           // }
+           // Console.WriteLine($"first index  = {scores.Keys[0]}");
+           // Console.WriteLine($"first value  = {scores.Values[0]}");
+           //bool check = scores.ContainsKey( 500 );
+           // scores.Remove(200);
+           // foreach (var entry in scores)
+           // {
+           //     Console.WriteLine($"{entry.Value} : {entry.Key}");
+           // }
             #endregion
         }
     }
