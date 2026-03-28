a6b05c9d5cbd6d5264f4718098ee6556f450ee38
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-03-28 02:05:32 +0200

    Exercise 4

--- Program.cs
+++ Program.cs
@@ -77,6 +77,30 @@ namespace G_Net_40_ADV03
             //Console.WriteLine(string.Join(", ", phonenumbers.Values));
 
             #endregion
+            #region Exercise 4
+    //        HashSet<string> email = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+    //        {"ahmed@test.com", "AHMED@test.com", "sara@test.com", "Sara@Test.Com" };
+    //        foreach (var item in email)
+    //        {
+    //            Console.WriteLine(item);
+    //        }
+    //        HashSet<int> A = new() { 1, 2, 3, 4, 5 };
+    //        HashSet<int> B = new() { 4, 5, 6, 7, 8 };
+          
+    //        A.UnionWith(B);
+    //        Console.WriteLine("Union: " + string.Join(", ", A));
+
+    //        A = new HashSet<int> { 1, 2, 3 , 4, 5 };
+
+    //        A.IntersectWith(B);
+    //        Console.WriteLine("Intersect: " + string.Join(", ", A));
+    //A = new HashSet<int> { 1, 2, 3 , 4 , 5 };
+    //        A.ExceptWith(B);
+    //        Console.WriteLine("Except: " + string.Join(", ", A));
+    //        A= new HashSet<int> { 1,2, 3 , 4 ,5 };
+    //    HashSet<int> C=new HashSet<int> { 1, 2 };
+    //        bool issubset = A.IsSubsetOf(C);
+            #endregion
         }
     }
 }
